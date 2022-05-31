77a9e597a84a3dec893412f7198d343743a1032b
LauG78 <byke.g20@gmail.com>
2022-05-31 16:57:32 +0200

    light/dark mode and hover pop images

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ function randomLetter(){
     return alphabet[Math.floor(Math.random() * alphabet.length)]
 }
 
+//carousel
+
 let images = [...document.querySelectorAll('.image_carousel')];
 let slider = document.querySelector('.slider');
 let sliderWidth;
@@ -48,3 +50,57 @@ function animateImages() {
    }
    init()
    animate()
+
+//light-dark mode
+
+let switchMode = false;
+document.querySelector('.mode').addEventListener('click', () => {
+    if (switchMode == false){
+        switchMode = true;
+        var body = document.querySelector('body');
+        body.setAttribute('class', 'darkMode');
+    }else{
+        switchMode = false;
+        document.querySelector('body').setAttribute('class', 'lightMode');
+    }
+})
+
+//chasing circle
+
+  let box = document.getElementById('chaserBox');
+  let chaser = document.getElementById('chaser');
+  
+  box.addEventListener('mousemove', (e) => {
+      let left = e.offsetX;
+      let top = e.offsetY;
+      chaser.style.left = `${left - 25}px`
+      chaser.style.top = `${top - 25}px`
+
+})
+
+
+
+// hover-pop-image
+
+const pokePop = Array.from(document.querySelectorAll('.poke'));
+pokePop.forEach((name) => {
+    name.addEventListener('mouseover', () => {
+        const sprite = new Image();
+        sprite.src = `../poke_pop_images/${name.innerText}.png`;
+        sprite.style.position = 'absolute';
+        sprite.style.left = '0';
+        sprite.style.transform = 'translateY(-100%)'
+        sprite.style.height = '400%'
+        if (name.contains(sprite)) return
+        if (name.hasChildNodes()) {
+            name.innerHTML = name.innerText;
+        }
+        name.appendChild(sprite)
+    })
+})
+pokePop.forEach((name) => {
+    name.addEventListener('mouseleave', () => {
+        name.innerHTML = name.innerText;
+
+    })
+})
